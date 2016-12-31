1493b86a365349c7c6280dc74cd0110ae6f2107f
king <573723831@qq.com>
2016-12-31 12:28:43 +0800

    succeeded

---
+++ app/src/main/java/com/example/choosepictest/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.choosepictest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button button;
+    public static final int TAKE_PHOTO=1;
+    public static final int CROP_PHOTO=2;
+    private Uri imageUri;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.take_photo);
+        imageView=(ImageView)findViewById(R.id.picture);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //创建File对象，用于存储拍照后的照片
+                File outputImage=new File(Environment.getExternalStorageDirectory(),"tempImage.jpg");
+                try{
+                    if (outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                imageUri=Uri.fromFile(outputImage);
+                Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestcode,int resultCode,Intent data){
+        switch (requestcode){
+            case TAKE_PHOTO:
+                if (resultCode==RESULT_OK){
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri,"image/*");
+                    intent.putExtra("scale", true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                    startActivityForResult(intent,CROP_PHOTO);
+                }
+                break;
+            case CROP_PHOTO:
+                if (resultCode== RESULT_OK){
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        imageView.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
